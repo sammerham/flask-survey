24af63e57c7bb87867b02f6d845c46db032b245a
Sam Merham <samehisaac@yahoo.com>
2021-04-22 15:49:37 -0700

    completed step2

--- app.py
+++ app.py
@@ -8,16 +8,25 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
 
 @app.route("/")
 def homepage():
     """return the html that shows survey title, instructions, and start button"""
     title = survey.title
     instructions = survey.instructions
-
+    session['responses'] = []
     return render_template("survey_start.html", 
     survey_title = title, 
     survey_instructions = instructions)
 
-@app.route("/begin/questions/<int:question_number>")
+
+@app.route("/begin",methods = ["POST"])
+def begin_survey():
+    """ Starts the survery"""
+    return redirect('/questions/0')
+
+
+@app.route('/questions/<int:question_num>')
+def display_questions(question_num):
+    question = survey.questions[question_num]
+    return render_template('question.html', question =question)
