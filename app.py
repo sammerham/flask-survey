605155a0a0f065ec525eac4667150cfcead59679
yceline-yu <cellystuff5@gmail.com>
2021-04-22 16:19:49 -0700

    step 3/4/5

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ def homepage():
     """return the html that shows survey title, instructions, and start button"""
     title = survey.title
     instructions = survey.instructions
-    session['responses'] = []
+    session["responses"] = []
+    session["question_number"] = 0
     return render_template("survey_start.html", 
     survey_title = title, 
     survey_instructions = instructions)
@@ -22,11 +23,25 @@ def homepage():
 
 @app.route("/begin",methods = ["POST"])
 def begin_survey():
-    """ Starts the survery"""
-    return redirect('/questions/0')
+    """ redirects to questions """
+    session["question_number"] = 0
+    return redirect(f"/questions/{session['question_number']}")
 
 
-@app.route('/questions/<int:question_num>')
+@app.route("/questions/<int:question_num>")
 def display_questions(question_num):
+    """ displays current quesstion"""
+
     question = survey.questions[question_num]
-    return render_template('question.html', question =question)
+    return render_template('question.html', question = question)
+
+@app.route("/answer", methods=["POST"])
+def store_answers():
+    """stores answer and redirects to next question"""
+    question_answer = request.form["answer"]
+    session["responses"].append(question_answer)
+    session["question_number"] += 1
+    if session["question_number"] >= len(survey.questions):
+        return render_template("completion.html")
+    else:
+        return redirect(f"/questions/{session['question_number']}")
